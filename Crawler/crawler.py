f8bb24ae16f3d925a61bc15b718eb8db8a8b06ff
Juan Tejada <juans.tejada@gmail.com>
2013-08-25 22:54:28 -0500

    Major restructural changes on repo. Modified manager.py to follow PEP8 conventions and cleaned up code a little

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,86 +1,197 @@
-#crawler v2
-import requests, json, sys
+#!/usr/bin/env python
+import sys
+import json
+import github3
+import logging
+import pika
+from github3.models import GitHubError
 from pymongo import MongoClient
-#ds041168.mongolab.com:41168/tesisv2
-persis = json.loads(open('data.json').read())
-idL = persis['last_id']#ultimoIdGuardado
-user = 0
-client = MongoClient('ds041168.mongolab.com', 41168)
-db = client.tesisv2
-db.authenticate('user', 'P4ssW0rd122#')
-coll = db.listRepo
-username = persis["users"][user]["user"]
-password = persis["users"][user]["pass"]
-link = 'https://api.github.com/repositories?since=%i' % (idL)
-while True:
-    print link
-    r = requests.get(link, auth=(username, password))
-    link = r.headers['link'][1:r.headers['link'].find('>')]
-    limit = r.headers['X-RateLimit-Remaining']
-    time = r.headers['x-ratelimit-reset']
-    if True: ## info
-        print r.status_code
-        print r.headers['link'][1:r.headers['link'].find('>')]
-        print r.headers['X-RateLimit-Remaining']
-        print r.headers['x-ratelimit-reset']
-        print r.encoding
+
+DATA_PATH = '../data'
+
+LANGUAGES = []
+
+RABBIT_HOST = ''
+
+MONGO_HOST = ''
+MONGO_PORT = 0
+MONGO_USER = ''
+MONGO_PWD = ''
+MONGO_DB = ''
+MONGO_COLL = ''
+
+GH_USERS = []
+GH_CUR_USR = 0
+
+
+def main(start_from=None):
+
+    load_config()
+    last_id = start_from
+    connection, channel = get_queue_channel(RABBIT_HOST)
+
+    while 1:
+        # Authenticate on GitHub and get all repos
+        gh = github3.login(GH_USERS[GH_CUR_USR]['login'], GH_USERS[GH_CUR_USR]['pwd'])
+        repos = gh.iter_all_repos(since=last_id)
+
+        # Connect to databse
+        client = MongoClient(MONGO_HOST, MONGO_PORT)
+        db = client[MONGO_DB]
+        db.authenticate(MONGO_USER, MONGO_PWD)
+        db_repos = db[MONGO_COLL]
+
+        # Crawl repos
+        last_id = start_crawl(repos, db_repos, gh, channel)
+
+        #Close connection to databse
+        client.close()
+
+    #Close connection to queue
+    channel.close()
+    connection.close()
+
+
+def start_crawl(repos, db_repos, gh, channel):
+    last_id = None
     try:
-        data = r.json()
-        print "dataRecive %i" % (len(data))
-        for repo in data:
-            print "id %i" % (repo["id"])
-            if coll.find_one({"id": repo["id"]}) != None: ## check si ya esta guardado
-                print "ya esta guardado %i" % repo["id"]  #hay algo actualizar
+        for r in repos:
+            repo = gh.repository(r.full_name.split('/')[0], r.full_name.split('/')[1])
+            json_repo = repo.to_json()
+            db_repo = db_repos.find_one({"_id": repo.id})
+            to_insert = {"_id": repo.id,
+                         "analyzed_at": None,
+                         "state": "pending",
+                         "name": repo.name,
+                         "full_name": repo.full_name,
+                         "description": repo.description,
+                         "fork": repo.fork,
+                         "created_at": repo.created_at,
+                         "updated_at": repo.updated_at,
+                         "homepage": repo.homepage,
+                         "size": repo.size,
+                         "language": repo.language,
+                         "has_issues": repo.has_issues,
+                         "has_downloads": repo.has_downloads,
+                         "has_wiki": repo.has_wiki,
+                         "forks": repo.forks,
+                         "open_issues": repo.open_issues,
+                         "watchers": repo.watchers,
+                         "url": json_repo[u'url'],
+                         "git_url": repo.git_url,
+                         "issues_url": json_repo[u'issues_url'],
+                         "collaborators_url": json_repo[u'collaborators_url'],
+                         "languages_url": json_repo[u'languages_url'],
+                         "archive_url": json_repo[u'archive_url'],
+                         "comments_url": json_repo[u'comments_url'],
+                         "contributors_url": json_repo[u'contributors_url'],
+                         "html_url": repo.html_url,
+                         "subscribers_url": json_repo[u'subscribers_url'],
+                         "stargazers_url": json_repo[u'stargazers_url'],
+                         "owner": {"html_url": json_repo[u'owner'][u'html_url'],
+                                   "type": json_repo[u'owner'][u'type'],
+                                   "repos_url": json_repo[u'owner'][u'repos_url']}
+                         }
+            last_id = repo.id
+
+            if db_repo:
+                del to_insert['_id']
+                to_insert['analyzed_at'] = db_repo.analyzed_at
+
+                if db_repo.state == "completed" and repo.updated_at > db_repo.analyzed_at:
+                    db_repos.update({"_id": last_id}, {"$set": to_insert})
+                    print "Updated repo with id", last_id
+                    push_to_queue(repo, channel)
+
+                else:
+                    to_insert['state'] = db_repo.state
+                    db_repos.update({"_id": last_id}, {"$set": to_insert})
+                    print "Updated repo with id", last_id
+
             else:
-                #new info
-                addInfo = requests.get(repo["url"], auth=(username, password))
-                add = addInfo.json()
-                limit = addInfo.headers['X-RateLimit-Remaining']
-                time = addInfo.headers['x-ratelimit-reset']
-                if (limit <= 1):                                #Cambia el user si se acabo el limit
-                    user += 1
-                    username = persis['users'][user]['user']
-                    password = persis['users'][user]['pass']
-                coll.insert({#sacaInfo y guarda
-                             "id": repo["id"],
-                             "url": repo["url"],
-                             "description": repo["description"],
-                             "downloads_url": repo["downloads_url"],
-                             "languages_url": repo["languages_url"],
-                             "git_url": add["git_url"],
-                             "name":add["name"],
-                             "full_name": add["full_name"],
-                             "languages": add["language"],
-                             "watchers_count": add["watchers_count"],
-                             "has_issues": add["has_issues"],
-                             "has_downloads": add["has_downloads"],
-                             "has_wiki": add["has_wiki"],
-                             "forks": add["forks"],
-                             "watchers": add["watchers"],
-                             "open_issues_count": add["open_issues_count"],
-                             "archive_url": repo["archive_url"],
-                             "owner": {
-                                 "following_url": repo["owner"]["following_url"],
-                                 "html_url": repo["owner"]["html_url"],
-                                 "starred_url": repo["owner"]["starred_url"],
-                                 "type": repo["owner"]["type"],
-                                 "followers_url": repo["owner"]["followers_url"],
-                                 "repos_url": repo["owner"]["followers_url"]
-                             },
-                             "html_url": repo["html_url"],
-                             "archive_url": repo["archive_url"],
-                             "events_url": repo["events_url"],
-                })
-                idL = repo["id"]
-                persis['last_id'] = idL
-                persis['users'][user]['limit'] = limit
-                persis['users'][user]['timeReset'] = time
-                with open('data.json', 'w') as outfile:
-                    json.dump(persis, outfile)
-            print "limite %s" % (limit)
+                db_repos.insert(to_insert)
+                print "Inserted repo with id", last_id
+                push_to_queue(repo, channel)
+
+    except GitHubError as e:
+        global GH_CUR_USR
+        print e
+        print "Limit reached, switching users"
+        GH_CUR_USR = (GH_CUR_USR + 1) % len(GH_USERS)
+        return last_id
     except:
-        persis['last_id'] = idL
-        with open('data.json', 'w') as outfile:
-            json.dump(persis, outfile)
-        print "error:", sys.exc_info()[0]
-        break
+        print "Unexpected error:", sys.exc_info()[0]
+        print "Restarting crawl from last inserted id"
+        return last_id
+
+
+def get_queue_channel(host):
+
+    # Necesary logging
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.CRITICAL)
+
+    # RabbitMQ connection
+    # server_credentials = pika.PlainCredentials('admin', 'openhub')
+    # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=server_credentials))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+
+    # Queue declaration
+    channel = connection.channel()
+    channel.exchange_declare(exchange='repo_classifier',
+                             type='direct')
+
+    return connection, channel
+
+
+def push_to_queue(repo, channel):
+    if repo.language in LANGUAGES:
+        body = "%s::%i::%s::%s" % (repo.git_url, repo.id, repo.full_name, repo.name)
+        channel.basic_publish(exchange='repo_classifier',
+                              routing_key=repo.language,
+                              body=body,
+                              properties=pika.BasicProperties(
+                                  delivery_mode=2,  # make message persistent
+                              ))
+        print "\tPushed to queue:", body
+    else:
+        print "\tCan't analyze language. Did not push to queue."
+
+
+def load_config():
+    global RABBIT_HOST, LANGUAGES, GH_USERS, MONGO_PWD, MONGO_USER, MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLL
+
+    rabbit_cfg = open(DATA_PATH + "/rabbit.json")
+    data = json.load(rabbit_cfg)
+    RABBIT_HOST = data['RABBIT_HOST']
+    rabbit_cfg.close()
+
+    langs = open(DATA_PATH + "/langs.json")
+    LANGUAGES = set(json.load(langs))
+    langs.close()
+
+    gh_cfg = open(DATA_PATH + "/gh.json")
+    GH_USERS = json.load(gh_cfg)
+    gh_cfg.close()
+
+    mongo_cfg = open(DATA_PATH + "/mongo.json")
+    data = json.load(mongo_cfg)
+    MONGO_USER = data['MONGO_USER']
+    MONGO_PWD = data['MONGO_PWD']
+    MONGO_HOST = data['MONGO_HOST']
+    MONGO_PORT = int(data['MONGO_PORT'])
+    MONGO_DB = data['MONGO_DB']
+    MONGO_COLL = data['MONGO_COLL']
+    mongo_cfg.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        arg = None
+        try:
+            arg = int(sys.argv[1])
+        except:
+            print "Usage: %s <numerical id to start from>" % __file__
+            sys.exit()
+        main(arg)
+    else:
+        main()
---
+++ worker/manager.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+import pika
+import logging
+import imp
+import glob
+import shutil
+import git
+import os
+import sys
+import json
+from pymongo import MongoClient
+
+DATA_PATH = '../data'
+
+BASE_DIR = 'zFileTemp'
+
+RABBIT_HOST = ''
+RABBIT_USER = ''
+RABBIT_PWD = ''
+RABBIT_KEY = ''
+RABBIT_QUEUE = ''
+
+MONGO_HOST = ''
+MONGO_PORT = 0
+MONGO_USER = ''
+MONGO_PWD = ''
+MONGO_DB = ''
+MONGO_COLL = ''
+
+dirs = []
+collection = ''
+
+
+def main():
+    global collection
+
+    load_config()
+
+    # Database connection
+    client = MongoClient(MONGO_HOST, MONGO_PORT)
+    db = client[MONGO_DB]
+    db.authenticate(MONGO_USER, MONGO_PWD)
+    collection = db[MONGO_COLL]
+
+    # Necesary logging
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.CRITICAL)
+
+    # RabbitMQ connection
+    # server_credentials = pika.PlainCredentials(RABBIT_USER, RABBIT_PWD)
+    # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=server_credentials))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST))
+
+    # Queue and Exchange declaration
+    channel = connection.channel()
+    channel.exchange_declare(exchange='repo_classifier', type='direct')
+    result = channel.queue_declare(queue=RABBIT_QUEUE, durable=True)
+    queue_name = result.method.queue
+
+    # Queue Binding
+    channel.queue_bind(exchange='repo_classifier', queue=queue_name, routing_key=RABBIT_KEY)
+
+    print ' [*] Waiting for messages. To exit press CTRL+C'
+
+    # Channel Options and Execution
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback, queue=queue_name)
+    channel.start_consuming()
+
+
+def down_repo(git_url, path):
+    """Download repo from specified url into path."""
+
+    if os.path.exists(path):
+        delete_repo(path)
+
+    firts = os.getcwd()
+    os.chdir("%s/%s/" % (firts, BASE_DIR))
+
+    print "Downloading code..."
+    print git.Git().clone(git_url)
+
+    os.chdir(firts)
+
+    print "Done"
+
+
+def delete_repo(path):
+    """Delete repo from path."""
+    shutil.rmtree(path)
+
+
+def callback(ch, method, properties, body):
+    """Queue callback function.
+    Will be executed when the queue pops this worker
+    """
+    print " [x] Received %r" % (body,)
+    data = body.split("::")
+    git_url = data[0]
+    repo_id = data[1]
+    name = data[2]
+    repoJson = {'_id': int(repo_id)}
+    print data
+    path = '%s/%s' % (BASE_DIR, name)
+    # try:
+    repoJson = collection.find_one({"_id": int(repo_id)})
+    down_repo(git_url, path)
+    for d in dirs:
+        print d
+        tests = glob.glob("%s/*.py" % d)  # Test list in the directories
+        print (tests)
+        for test in tests:
+            try:
+                m = imp.load_source(test, test)
+                res = m.runTest(repo_id, path, data)
+                name = m.name
+                repoJson[d] = []
+                data = "{'name':'%s','value':'%s'}}" % (name, res)
+                repoJson[d].append(data)
+            except Exception as e:
+                print 'Test error %s %s' % (d, test)
+                print str(e)
+                print "Error:", sys.exc_info()
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print "Save results..."
+    delete_repo(path)
+    print "Delete files..."
+    collection.update({"_id": int(repo_id)}, repoJson)
+    print "Ready, waiting for next repo..."
+    # except:
+    #     print "General error"
+    #     collection.update({"id": int(repo_id)} , repoJson)
+    #     print "error:", sys.exc_info()
+    #     #TODO Terminarlos errores
+    # except:
+    #     print sys.exc_info()
+    #     print "ERRORR"
+
+# def initRabbitMq(host="localhost", queue="task_queue", key="Python"):
+#     print host
+#     print queue
+#     print key
+
+
+def load_config():
+    global RABBIT_HOST, RABBIT_USER, RABBIT_PWD, RABBIT_KEY, RABBIT_QUEUE, MONGO_PWD, MONGO_USER, MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLL, dirs
+
+    rabbit_cfg = open(DATA_PATH + "/rabbit.json")
+    data = json.load(rabbit_cfg)
+    RABBIT_HOST = data['RABBIT_HOST']
+    RABBIT_USER = data['RABBIT_USER']
+    RABBIT_PWD = data['RABBIT_PWD']
+    RABBIT_KEY = data['RABBIT_KEY']
+    RABBIT_QUEUE = data['RABBIT_QUEUE']
+    rabbit_cfg.close()
+
+    d = open(DATA_PATH + "/analyzers.json")
+    dirs = set(json.load(d))
+    d.close()
+
+    mongo_cfg = open(DATA_PATH + "/mongo.json")
+    data = json.load(mongo_cfg)
+    MONGO_USER = data['MONGO_USER']
+    MONGO_PWD = data['MONGO_PWD']
+    MONGO_HOST = data['MONGO_HOST']
+    MONGO_PORT = int(data['MONGO_PORT'])
+    MONGO_DB = data['MONGO_DB']
+    MONGO_COLL = data['MONGO_COLL']
+    mongo_cfg.close()
+
+
+if __name__ == '__main__':
+    main()
