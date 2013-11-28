ce8ee2a3ef322cf4eb24e35e46a1c0d8cb684c20
gabrielfarah <gabomagno03@gmail.com>
2013-11-27 19:04:13 -0500

    Fixes small

--- worker/usability/documentation_analysis/main.py
+++ worker/usability/documentation_analysis/main.py
@@ -14,7 +14,7 @@ def features_extraction(clean_html):
     result_list = ""
     soup = BeautifulSoup(clean_html)
     #------------------------------- text = ''.join(soup.findAll(text=True))
-    tags = ['h2','h3','h4','h5','h6']
+    tags = ['b','h1','h2','h3','h4','h5','h6']
     for link in soup.find_all(tags):
         temp_text = link.text
         for ch in ['&','#','_','?','.','-','1','2','3','4','5','6','7','8','9','0']:
@@ -27,7 +27,7 @@ def load_and_clear_file(local_path):
     with open(local_path, 'r') as content_file:
         content = content_file.read()
     content_file.close()
-    return markdown(content)
+    return markdown(content.decode('utf-8'))
 
 def run_test(id, path, repo_db):
     for root, subFolders, files in os.walk(path):
@@ -37,7 +37,7 @@ def run_test(id, path, repo_db):
                 clean_html = load_and_clear_file(os.path.join(root, f))
                 result_list = features_extraction(clean_html)
                 #Load the saved classifier
-                classifier = joblib.load(os.path.dirname(os.path.realpath(__file__))+'/pickles/classifier.pkl')
+                classifier = joblib.load(os.path.dirname(os.path.realpath(__file__))+'/pickles/model.pkl')
                 #convert our list of headers to an array
                 X_test = np.array([result_list])
                 target_names = ['Bad', 'Average', 'Good']
---
+++ worker/usability/documentation_analysis/training_data/bad/README.markdown
@@ -0,0 +1,19 @@
+#### A service that creates your resume based on your GitHub repos.
+
+Possible Usecases:
+
+ * Boon for all the tech-savy bosses who want to have a **quick view** of person's git/github activity, _before the interview_
+
+### Opt-In
+
+GitHub Resume is now opt-in rather than opt-out! To make your resume visible
+just **star** this project, [resume/resume.github.com](https://github.com/resume/resume.github.com).
+That's all there is to it!
+
+### For Developers
+
+To run the app in development mode:
+
+    rackup config.ru
+
+(You must have Ruby and the rack gem installed.)
